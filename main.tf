9d0d4dbec16372c2534e659e20a9eb9b2e3b9cdc
srakesh28 <srakesh28@yahoo.com>
2017-06-26 19:18:33 -0700

    added sample files

---
+++ main.tf
@@ -0,0 +1,37 @@
+#main.tf
+# Configure the Microsoft Azure Provider
+provider "azurerm" {  
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "development" {
+  name     = "development"
+  location = "West US"
+}
+
+# Create a virtual network in the web_servers resource group
+resource "azurerm_virtual_network" "network" {
+  name                = "develoNetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = "West US"
+  resource_group_name = "${azurerm_resource_group.development.name}"
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+#variables.tf
+variable "tenant_id" {  
+    default = "<your tenant_id>"
+}
+
+variable "client_id" {  
+    default="<your client_id"
+}
+
+variable "client_secret" {  
+    default="<your client_secret"
+}
+
+variable "subscription_id" {  
+    default="your subscription_id"
+}
